eef5d93be54fe3176d0b466c50d0d4f16e666013
Muhammad Idrees <adreesdev@gmail.com>
2022-11-02 16:05:18 +0500

    router dom

--- src/components/Refferal/Main.jsx
+++ src/components/Refferal/Main.jsx
@@ -1,9 +1,42 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Main = () => {
 	return (
 		<>
-			<>aaa</>
+			<Box py={5}>
+				<Box
+					sx={{
+						display: "flex",
+						borderBottom: "2px solid #F1620A",
+						width: "260px",
+					}}
+				>
+					<Typography
+						sx={{
+							fontFamily: "'Russo One'",
+							fontStyle: "normal",
+							fontWeight: "400",
+							fontSize: "48px",
+							lineHeight: "58px",
+							color: "#F1620A",
+						}}
+					>
+						SEEK
+					</Typography>
+					<Typography
+						sx={{
+							fontFamily: "'Russo One'",
+							fontStyle: "normal",
+							fontWeight: "400",
+							fontSize: "48px",
+							lineHeight: "58px",
+						}}
+					>
+						GOLD
+					</Typography>
+				</Box>
+			</Box>
 		</>
 	);
 };
